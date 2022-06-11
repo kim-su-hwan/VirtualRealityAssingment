ef78d30e49ca5e4cb329d2c4ba6b8ac7510114af
pantokrator <pantokrator3841@gmail.com>
2022-06-11 17:08:22 +0900

    sdf

--- Assets/Scripts/GamePlaySceneScripts/RecipesManager.cs
+++ Assets/Scripts/GamePlaySceneScripts/RecipesManager.cs
@@ -46,7 +46,7 @@ public class RecipesManager : MonoBehaviour
         if(m_code == 0)
         {
             // Americano
-            tmp.text = "Americano recipe is in here.";
+            tmp.text = "Iced Americano : Ice -> Espresso -> Water (E1:W6).\n" + "Hot Americano : Water -> Espresso (E1:W6)";
         }
         if (m_code == 1)
         {
@@ -61,12 +61,19 @@ public class RecipesManager : MonoBehaviour
         if (m_code == 3)
         {
             // Mocha Latte
-            tmp.text = "Mocha Latte recipe is in here.";
+            tmp.text = "Iced Mocha Latte : Mocha Syrup -> Espresso -> Ice -> Milk ->(MS1:E3:M10).\n" + "Hot Mocha Latte : Mocha Syrup -> Espresso -> Milk (MS1:E3:M10)";
         }
         if (m_code == 4)
         {
             // Ade
             tmp.text = "Ade recipe is in here.";
         }
+// 아이스 카페라떼 : 얼음 -> 우유 -> 에스프레소 (비율 => 에스프레소(2shot) : 우유 = 1 : 4)
+// 핫 카페라떼 : 우유 -> 에스프레소 (비율 => 에스프레소 : 우유 = 1 : 5)
+// 아이스 바닐라라떼 : 바닐라시럽 -> 에스프레소 -> 얼음 -> 우유 (비율 => 시럽 : 에스프레소 : 우유 = 1 : 3 : 10)
+// 핫 바닐라라떼 : 바닐라시럽 -> 에스프레소 -> 우유 (비율 => 시럽 : 에스프레소 : 우유 = 1 : 3 : 10)
+// 딸기 에이드 : 딸기청 -> 얼음 -> 사이다 (비율 => 딸기청 : 사이다 = 1 : 5)
+// 아이스 녹차라떼 : 녹차가루 -> 물 -> 얼음 -> 우유 (비율 => 녹차베이스(물+녹차가루(1팩)) : 우유 = 1 : 5)
+
     }
 }
