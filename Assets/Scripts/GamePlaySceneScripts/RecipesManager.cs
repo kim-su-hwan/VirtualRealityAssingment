d2672547a350d18142cc2844cf88211ab36df476
pantokrator <pantokrator3841@gmail.com>
2022-06-12 01:09:43 +0900

    Update RecipesManager.cs

--- Assets/Scripts/GamePlaySceneScripts/RecipesManager.cs
+++ Assets/Scripts/GamePlaySceneScripts/RecipesManager.cs
@@ -46,34 +46,34 @@ public class RecipesManager : MonoBehaviour
         if(m_code == 0)
         {
             // Americano
-            tmp.text = "Iced Americano : Ice -> Espresso -> Water (E1:W6).\n" + "Hot Americano : Water -> Espresso (E1:W6)";
+            tmp.text = "Iced Americano\nIce -> Espresso -> Water\n(E1:W6)\n\nHot Americano\nWater -> Espresso\n(E1:W6)";
         }
         if (m_code == 1)
         {
             // Caffe Latte
-            tmp.text = "Caffe Latte recipe is in here.";
+            tmp.text = "Iced Caffe Latte\nIce -> Milk -> Espresso\n(E(2shots)1:M4)\n\n" + "Hot Caffe Latte\nMilk -> Espresso\n(E1:M5)";
         }
         if (m_code == 2)
         {
             // Vanilla Latte
-            tmp.text = "Vanilla Latte recipe is in here.";
+           tmp.text = "Iced Vanilla Latte\nVanilla Syrup -> Espresso -> Ice -> Milk\n(VS1:E3:M10)\n\n" + "Hot Vanilla Latte\nVanilla Syrup -> Espresso -> Milk\n(VS1:E3:M10).";
         }
         if (m_code == 3)
         {
             // Mocha Latte
-            tmp.text = "Iced Mocha Latte : Mocha Syrup -> Espresso -> Ice -> Milk ->(MS1:E3:M10).\n" + "Hot Mocha Latte : Mocha Syrup -> Espresso -> Milk (MS1:E3:M10)";
+            tmp.text = "Iced Mocha Latte\nMocha Syrup -> Espresso -> Ice -> Milk\n(MS1:E3:M10)\n\n" + "Hot Mocha Latte\nMocha Syrup -> Espresso -> Milk\n(MS1:E3:M10)";
         }
         if (m_code == 4)
         {
-            // Ade
-            tmp.text = "Ade recipe is in here.";
+            // Strawberry Ade
+            tmp.text = "Iced Strawberry Ade\nStrawberry Puree -> Ice -> Soda\n(SP1:S5)";
         }
-// 아이스 카페라떼 : 얼음 -> 우유 -> 에스프레소 (비율 => 에스프레소(2shot) : 우유 = 1 : 4)
-// 핫 카페라떼 : 우유 -> 에스프레소 (비율 => 에스프레소 : 우유 = 1 : 5)
-// 아이스 바닐라라떼 : 바닐라시럽 -> 에스프레소 -> 얼음 -> 우유 (비율 => 시럽 : 에스프레소 : 우유 = 1 : 3 : 10)
-// 핫 바닐라라떼 : 바닐라시럽 -> 에스프레소 -> 우유 (비율 => 시럽 : 에스프레소 : 우유 = 1 : 3 : 10)
-// 딸기 에이드 : 딸기청 -> 얼음 -> 사이다 (비율 => 딸기청 : 사이다 = 1 : 5)
-// 아이스 녹차라떼 : 녹차가루 -> 물 -> 얼음 -> 우유 (비율 => 녹차베이스(물+녹차가루(1팩)) : 우유 = 1 : 5)
+        if (m_code == 5)
+        {
+            tmp.text = "Iced Green Tea Latte\nGreen Tea Powder -> Ice -> Milk\n(G1:M5)";
+        }
+        // 딸기 에이드 : 딸기청 -> 얼음 -> 사이다 (비율 => 딸기청 : 사이다 = 1 : 5)
+        // 아이스 녹차라떼 : 녹차가루 -> 물 -> 얼음 -> 우유 (비율 => 녹차베이스(물+녹차가루(1팩)) : 우유 = 1 : 5)
 
     }
 }
